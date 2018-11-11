46d911da6719ae2069ed4e87bdcc6922c21459a5
gin2018 <gin2018@github.com>
2018-11-11 23:13:52 +0800

    add sqlite

---
+++ sqlite/cmake-build-debug/CMakeFiles/sqlite.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sqlite.dir/main.cpp.o"
+  "sqlite.pdb"
+  "sqlite"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sqlite.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
